229dd04663eb74d5ccef974c69719dcaa16d6d20
ZachTaylor99 <zach@pilotweststudios.com>
2025-04-28 15:38:25 -0500

    Add articles listing feature
    hid some old code from the generate function and added it in manually

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
+  # resources :articles
   root "pages#home"
   get "about", to: "pages#about"
+  resources :articles, only: [ :show, :index ]
 end
