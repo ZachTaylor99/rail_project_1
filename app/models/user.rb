92f4aadba0ad09bd0fcacbf730d4d6a8f2053939
ZachTaylor99 <zach@pilotweststudios.com>
2025-06-09 12:06:23 -0500

    Added User validation
    updated validation and got email working also.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
-require "valid_email"
-
 class User < ApplicationRecord
+  has_many :articles
+
+  require "valid_email"
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, email: true
 end
