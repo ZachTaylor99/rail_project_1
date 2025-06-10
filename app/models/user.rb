9148c10e4dedca4007b2b3e01a027ae9e7af0c22
ZachTaylor99 <zach@pilotweststudios.com>
2025-06-10 11:39:02 -0500

    Added password security
    also added making sure emails are the same if downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
 
   require "valid_email"
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, email: true
+  has_secure_password
 end
