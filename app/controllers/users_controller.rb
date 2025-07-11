9c00e30ae0bc601a14858232658b2c4c34e0dc56
ZachTaylor99 <zach@pilotweststudios.com>
2025-07-11 11:56:32 -0500

    updated controller and helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update ]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -13,12 +14,19 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
-
+  def update
+    if @user.update(user_params)
+    flash[:notice] = "Your account was successfully updated"
+    redirect_to @user
+    else
+    render "edit"
+    end
+  end
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Website #{@user.username}, you have successfully signed up."
       redirect_to articles_path
     else
@@ -29,5 +37,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
