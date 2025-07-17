87a54424bc9e3f0235aeaa18ff1747a9f1d84b16
ZachTaylor99 <zach@pilotweststudios.com>
2025-07-17 15:15:20 -0500

    Updated create_category_test
    added additional testing to test invalid category submissions

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -12,4 +12,14 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_match "prevented", response.body
+    assert_select "h2.text-lg"
+    assert_select "ul.mt-2"
+  end
 end
