dad648b5ce1f48e61287693e6a487c71e78ca148
ZachTaylor99 <zach@pilotweststudios.com>
2025-07-18 13:59:36 -0500

    many to many back end
    added many to many association on the backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_16_172302) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_18_184554) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
